f95a30e3e5e5835ebc1fefb34158c02ea806daa2
G1 <1910285@student.uwtsd.ac.uk>
2024-03-29 12:12:48 +0000

    .

--- src/go.js
+++ src/go.js
@@ -2,4 +2,19 @@ const parseXML = require('./xmlParser'); // Assuming xmlParser.js is in the same
 
 const xmlFilePath = 'FHIR/Patient.schema.xml'; // Provide the path to your XML file
 const parsedData = parseXML(xmlFilePath);
-console.log(parsedData);
+
+
+for (const elementName in parsedData) {
+    if (parsedData.hasOwnProperty(elementName)) {
+        console.log("Element Name:", elementName);
+        const attributes = parsedData[elementName];
+        for (const attributeName in attributes) {
+            if (attributes.hasOwnProperty(attributeName)) {
+                const attributeValue = attributes[attributeName];
+                console.log("Attribute Name:", attributeName, ", Value:", attributeValue);
+            }
+        }
+    }
+}
+
+//console.log(parsedData);
