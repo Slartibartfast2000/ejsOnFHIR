45355a51a061d7eb1bd1726ef2340362f4718cd3
Slartibartfast <1910285@student.uwtsd.ac.uk>
2024-06-12 14:57:53 +0100

    .

--- web-server/controllers/fhirController.js
+++ web-server/controllers/fhirController.js
@@ -5,10 +5,10 @@ export async function readResource(req, res) {
     console.debug("fhirController.getResource:");
     
     const { resourceType, id } = req.params;
-    const baseUrl = 'http://localhost:8080/fhir'; // Replace with your actual FHIR API base URL
+    const fhirBaseUrl = 'http://localhost:8080/fhir'; // Replace with your actual FHIR API base URL
 
     console.debug("getResource:", resourceType, id);
-    const url = `${baseUrl}/${resourceType}/${id}`;
+    const url = `${fhirBaseUrl}/${resourceType}/${id}`;
     console.debug('getResource: ', url);
 
     try {
@@ -20,12 +20,22 @@ export async function readResource(req, res) {
 }
 
 export async function searchResource( req,res) {
-
+    //http://localhost:8080/fhir/Patient?family=Doe&given=John
     const { resourceType } = req.params;
     const queryString = req.query;
 
     console.log('Resource Type:', resourceType);
     console.log('Query String:', queryString);
+    
+    const url = `${fhirBaseUrl}/${resourceType}/${id}`;
+    
+    try {
+        const response = await axios.get(url);
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).json({ message: `Error fetching resource: ${error.message}` });
+    }
+
 }
 
 export default {readResource, searchResource};
