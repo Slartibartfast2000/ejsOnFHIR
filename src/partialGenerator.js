6ef51e03c69060bc04b76c53284eb90fa7981799
Slartibartfast <1910285@student.uwtsd.ac.uk>
2024-06-24 13:58:05 +0100

    .

--- src/partialGenerator.js
+++ src/partialGenerator.js
@@ -43,20 +43,40 @@ console.debug("reading schema: ", myparameters.xsdFilePath);
 const parsedData = parseXMLSchema(myparameters.xsdFilePath);
 console.debug("reading schema: done.");
 ///xs:schema/xs:complexType/@name
+const resourceType = "Encounter";
 
-const xpathQuery = '//xs:complexType[@name="Encounter"]/xs:complexContent/xs:extension/xs:sequence/*';
+const xpathQuery = `//xs:complexType[@name="Patient"]/xs:complexContent/xs:extension/xs:sequence/*`;
 // const xpathQuery = '//xs:complexType[@name="Patient"]/xs:complexContent/xs:extension/xs:sequence/*';
 
 const elements = runXPathQuery(parsedData, xpathQuery);
 
+let partialEJS = 
+`<div>
+<form id="${resourceType}">
+`;
 
+//partialEJS += "<form>" + crlf;
 // Output the selected elements
 elements.forEach((element) => {
-   console.log(element.toString());
+    console.log(element.toString());
+    const attributes = element.attributes;
+    for (let i = 0; i < attributes.length; i++) {
+        const attr = attributes[i];
+        console.debug(`     ${attr.nodeName}: ${attr.nodeValue}`);
+    }
+    const name = element.getAttribute('name');
+
+    partialEJS += `<input type='text' id='${resourceType}.${name}' name='${name}' placeholder='.'><br>"` + crlf;
 });
+
+partialEJS += 
+    `</form>
+    <div>`;
+console.debug(partialEJS);
+
 process.exit(0);
 
-let myPartialEJS= "";
+let myPartialEJS = "";
 
 myPartialEJS += "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><title>My HTML Page</title><style></style></head><body>" + crlf;
 
@@ -64,7 +84,7 @@ myPartialEJS += "<div><form><table>" + crlf + tab;
 
 for (const elementName in parsedData) {
     if (parsedData.hasOwnProperty(elementName)) {
-       // console.log("Element Name:", elementName);
+        // console.log("Element Name:", elementName);
 
         myPartialEJS += "<label for='text-input'>" + elementName + ":</label>" + crlf;
 
@@ -74,7 +94,7 @@ for (const elementName in parsedData) {
         for (const attributeName in attributes) {
             if (attributes.hasOwnProperty(attributeName)) {
                 const attributeValue = attributes[attributeName];
-               // console.log("Attribute Name:", attributeName, ", Value:", attributeValue);
+                // console.log("Attribute Name:", attributeName, ", Value:", attributeValue);
             }
         }
     }
